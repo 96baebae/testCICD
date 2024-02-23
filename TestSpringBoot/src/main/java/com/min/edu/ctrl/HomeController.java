718990f986322223d9dfddbde36ddb1a29a4bf19
user <user@DESKTOP-HDBL5LG.mshome.net>
2024-02-23 09:20:42 +0900

    2024.02.23
    TestSpringBoot 프로젝트 Commit and Push

---
+++ TestSpringBoot/src/main/java/com/min/edu/ctrl/HomeController.java
@@ -0,0 +1,12 @@
+package com.min.edu.ctrl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping(value = "/")
+	public String init() {
+		return "index.jsp";
+	}
+}
